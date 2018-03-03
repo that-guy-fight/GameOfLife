24b78cc1b7394dbd3dd9d93839a828495f5325e0
Fightie <that.guy.fight@gmail.com>
2018-03-02 21:53:38 -0500

    added in a copy of the 2d array

--- src/app/blinky-manager/blinky-manager.component.ts
+++ src/app/blinky-manager/blinky-manager.component.ts
@@ -8,16 +8,22 @@ import { NgModel } from '@angular/forms';
 })
 export class BlinkyManagerComponent implements OnInit, OnDestroy {
   userNumber: number;
-  childReferences: any[][];
+  blinkyReferences: any[][];
   myInterval: any;
   myTimer: any;
+  referencesCopy: any[][];
   maxRange = 1;
+  canActivate: boolean;
+  hasSubmitted: boolean;
+  isRunning = { state: false };
 
   constructor() { }
 
   ngOnInit() {
-    this.userNumber = 0;
-    this.childReferences = [];
+    this.blinkyReferences = [];
+    this.referencesCopy = [];
+    this.canActivate = false;
+    this.hasSubmitted = false;
   }
 
   ngOnDestroy() {
@@ -27,80 +33,108 @@ export class BlinkyManagerComponent implements OnInit, OnDestroy {
   }
 
   submitInput() {
-    this.initializeArrays();
+    if (!this.checkUserNumberInput()) {
+      //bad user input
+    } else {
+      this.canActivate = true;
+      this.hasSubmitted = true;
+      this.initializeArrays();
+    }
+  }
+
+  initializeArrays() {
+    this.blinkyReferences = [];
+    for (let i = 0; i < this.userNumber; i++) {
+      this.blinkyReferences[i] = [];
+      for (let j = 0; j < this.userNumber; j++) {
+        this.blinkyReferences[i][j] = { x: i, y: j, alive: false };
+      }
+    }
   }
 
   activate() {
+    this.canActivate = false;
+    this.isRunning.state = true;
+    this.referencesCopy = this.cloneObject(this.blinkyReferences);
     this.myInterval = setInterval(() => {
       this.calculate();
     }, 1000);
   }
 
-  initializeArrays() {
-    this.childReferences = [];
-    for (let i = 0; i < this.userNumber; i++) {
-      this.childReferences[i] = [];
-      for (let j = 0; j < this.userNumber; j++) {
-        this.childReferences[i][j] = {x: i, y: j, alive: false};
-      }
+  stop() {
+    this.canActivate = true;
+    this.isRunning.state = false;
+    if (this.myInterval) {
+      clearInterval(this.myInterval);
     }
   }
 
   calculate() {
-    for (let i = 0; i < this.childReferences.length; i++) {
-      for (let j = 0; j < this.childReferences[i].length; j++) {
-          this.childReferences[i][j].alive = this.checkNeighbors(i, j);
+    for (let i = 0; i < this.referencesCopy.length; i++) {
+      for (let j = 0; j < this.referencesCopy[i].length; j++) {
+        this.checkNeighbors(i, j);
       }
     }
+    this.blinkyReferences = this.cloneObject(this.referencesCopy);
   }
 
   checkNeighbors(i: number, j: number): boolean {
-    if (this.childReferences[i][j].alive === false) {
-      return this.checkDeadCell(i, j);
+    return this.checkCell(i, j, this.referencesCopy[i][j].alive);
+  }
+
+  checkCell(i: number, j: number, alive: boolean): boolean {
+    let count = 0;
+    for (let x = i - this.maxRange; x <= i + this.maxRange; x++) {
+      if (x < 0 || x >= this.referencesCopy.length) {
+        continue;
+      }
+      for (let y = j - this.maxRange; y <= j + this.maxRange; y++) {
+        if (y < 0 || y >= this.referencesCopy[x].length) {
+          continue;
+        }
+        if (this.checkForValidCell(i, j, x, y) && this.blinkyReferences[x][y].alive === true) {
+          count++;
+        }
+        if (count > 3) { return false; }
+      }
+    }
+    if (alive) {
+      if (count === 2 || count === 3) { return true; }
     } else {
-      return this.checkLiveCell(i, j);
+      if (count === 3) { return true; }
     }
-  }
-
-  checkDeadCell(i: number, j: number): boolean {
-    let count = 0;
-    for (let x = i - this.maxRange; x <= i + this.maxRange; x++) {
-      if (x < 0 || x >= this.childReferences.length) {
-        continue;
-      }
-      for (let y = j - this.maxRange; y <= j + this.maxRange; y++) {
-        if (y < 0 || y >= this.childReferences[x].length) {
-          continue;
-        }
-          if ((x !== i || y !== j) && this.childReferences[x][y] !== undefined
-            && this.childReferences[x][y] !== null && this.childReferences[x][y].alive === true) {
-          count++;
-        }
-        if (count > 3) { return false; }
-      }
-    }
-    if (count === 3) { return true; }
     return false;
   }
 
-  checkLiveCell(i: number, j: number): boolean {
-    let count = 0;
-    for (let x = i - this.maxRange; x <= i + this.maxRange; x++) {
-      if (x < 0 || x >= this.childReferences.length) {
-        continue;
-      }
-      for (let y = j - this.maxRange; y <= j + this.maxRange; y++) {
-        if (y < 0 || y >= this.childReferences[x].length) {
-          continue;
-        }
-          if ((x !== i || y !== j) && this.childReferences[x][y] !== undefined
-            && this.childReferences[x][y] !== null && this.childReferences[x][y].alive === true) {
-          count++;
-        }
-        if (count > 3) { return false; }
+  checkForValidCell(i: number, j: number, x: number, y: number): boolean {
+    if ((x !== i || y !== j) && this.referencesCopy[x][y] !== undefined
+      && this.referencesCopy[x][y] !== null) {
+      return true;
+    }
+    return false;
+  }
+
+  checkInputKey(event: any) {
+    if (event.key === 'Enter' && this.userNumber !== undefined) {
+      this.submitInput();
+    } else {
+      const numberPattern = /^[0-9]$/;
+      if (!numberPattern.test(event.key)) {
+        event.preventDefault();
       }
     }
-    if (count === 2 || count === 3) { return true; }
-    return false;
+  }
+
+  checkUserNumberInput(): boolean {
+    const validPattern = /^[0-9]+$/;
+    if (!validPattern.test(this.userNumber.toString())) {
+      //bad user input
+      return false;
+    }
+    return true;
+  }
+
+  cloneObject(obj: any): any {
+    return JSON.parse(JSON.stringify(obj));
   }
 }
--- src/app/blinky/blinky.component.ts
+++ src/app/blinky/blinky.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class BlinkyComponent implements OnInit {
   @Input('reference') reference: any;
+  @Input('isRunning') isRunning: any;
   alive: boolean;
 
   constructor() { }
